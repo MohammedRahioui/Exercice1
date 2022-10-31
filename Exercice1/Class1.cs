2365b12ca46e43044fd1b103fbc6c14ed4b46d1c
HP <HP@DESKTOP-POLSQSD>
2022-10-30 21:27:42 -1200

    Add project files.

---
+++ Exercice1/Class1.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+namespace Exercice1
+    {
+        class Premier
+        {
+            static void Main(string[] args)
+            {
+                int  nbr;
+                bool Est_premier;
+                System.Console.Write("Entre nomber ");
+                n = Int32.Parse(System.Console.ReadLine());
+                System.Console.WriteLine();
+
+                
+                          
+                        }
+                    if (Est_premier)
+                    {
+                        compt++;
+                        System.Console.WriteLine(nbr);
+                    }
+                }
+            }
+        }
+    }
+
